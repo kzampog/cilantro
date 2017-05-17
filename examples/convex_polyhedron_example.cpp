b744ba319e5db443c2e0be70d203aa7c0f591e6f
Konstantinos Zampogiannis <kzampog@gmail.com>
2017-05-17 15:49:16 -0400

    Working on qhull...

--- examples/convex_polyhedron_example.cpp
+++ examples/convex_polyhedron_example.cpp
@@ -1,34 +1,64 @@
 #include <cilantro/convex_polyhedron.hpp>
 #include <cilantro/ply_io.hpp>
+#include <cilantro/visualizer.hpp>
 
 #include <iostream>
 
 int main(int argc, char ** argv) {
 
-//    PointCloud cloud;
-//    readPointCloudFromPLYFile(argv[1], cloud);
+    PointCloud cloud;
+    readPointCloudFromPLYFile(argv[1], cloud);
 
-    std::vector<Eigen::Vector3f> points;
-    points.push_back(Eigen::Vector3f(0,0,0));
-    points.push_back(Eigen::Vector3f(1,0,0));
-    points.push_back(Eigen::Vector3f(0,1,0));
-    points.push_back(Eigen::Vector3f(0,0,1));
-//    points.push_back(Eigen::Vector3f(0,1,1));
-//    points.push_back(Eigen::Vector3f(1,0,1));
-//    points.push_back(Eigen::Vector3f(1,1,0));
-//    points.push_back(Eigen::Vector3f(1,1,1));
+    std::vector<Eigen::Vector4f> faces1 = VtoH<Eigen::Vector3f,Eigen::Vector4f>(cloud.points);
+    std::vector<Eigen::Vector3f> points2 = HtoV<Eigen::Vector4f,Eigen::Vector3f,Eigen::Vector3f>(faces1, Eigen::Vector3f(0,0,0.8));
 
-//    VtoH(points);
+    Visualizer vis("win", "disp");
+    vis.addPointCloud("initial", cloud, Visualizer::RenderingProperties().setDrawingColor(1,0,0).setOverrideColors(true).setOpacity(0.5).setPointSize(1.0));
+    vis.addPointCloud("final", points2, Visualizer::RenderingProperties().setDrawingColor(0,0,1));
 
-    std::vector<Eigen::Vector4f> faces;
-    faces.push_back(Eigen::Vector4f(-1,0,0,0));
-    faces.push_back(Eigen::Vector4f(0,-1,0,0));
-    faces.push_back(Eigen::Vector4f(0,0,-1,0));
-    faces.push_back(Eigen::Vector4f(1,1,1,-1));
+    while (!vis.wasStopped()) {
+        vis.spinOnce();
+    }
 
-    Eigen::Vector3f interior_pt(0.1, 0.1, 0.1);
-
-    HtoV(faces, interior_pt);
+//    std::vector<Eigen::Vector3f> points1;
+//    points1.push_back(Eigen::Vector3f(0,0,0));
+//    points1.push_back(Eigen::Vector3f(1,0,0));
+//    points1.push_back(Eigen::Vector3f(0,1,0));
+//    points1.push_back(Eigen::Vector3f(0,0,1));
+//    points1.push_back(Eigen::Vector3f(0,1,1));
+//    points1.push_back(Eigen::Vector3f(1,0,1));
+//    points1.push_back(Eigen::Vector3f(1,1,0));
+//    points1.push_back(Eigen::Vector3f(1,1,1));
+//
+//    std::cout << "V to H:" << std::endl;
+//    std::vector<Eigen::Vector4f> faces1 = VtoH<Eigen::Vector3f,Eigen::Vector4f>(points1);
+//    for (size_t i = 0; i < faces1.size(); ++i) {
+//        std::cout << faces1[i].transpose() << std::endl;
+//    }
+//
+////    std::vector<Eigen::Vector4f> faces2;
+////    faces2.push_back(Eigen::Vector4f(-1,0,0,0));
+////    faces2.push_back(Eigen::Vector4f(0,-1,0,0));
+////    faces2.push_back(Eigen::Vector4f(0,0,-1,0));
+////    faces2.push_back(Eigen::Vector4f(1,1,1,-1));
+//
+////    Eigen::Vector3f interior_pt(0.1, 0.1, 0.1);
+//
+//    Eigen::Vector3f interior_pt(0.5, 0.5, 0.5);
+//
+//    std::cout << "H to V:" << std::endl;
+//    std::vector<Eigen::Vector3f> points2 = HtoV<Eigen::Vector4f,Eigen::Vector3f,Eigen::Vector3f>(faces1, interior_pt);
+//    for (size_t i = 0; i < points2.size(); ++i) {
+//        std::cout << points2[i].transpose() << std::endl;
+//    }
+//
+////    Visualizer vis("win", "disp");
+////    vis.addPointCloud("initial", points1, Visualizer::RenderingProperties().setDrawingColor(1,0,0));
+////    vis.addPointCloud("final", points2, Visualizer::RenderingProperties().setDrawingColor(0,0,1));
+////
+////    while (!vis.wasStopped()) {
+////        vis.spinOnce();
+////    }
 
     return 0;
 }
--- include/cilantro/convex_polyhedron.hpp
+++ include/cilantro/convex_polyhedron.hpp
@@ -1,14 +1,76 @@
 #pragma once
 
-#include <libqhullcpp/RboxPoints.h>
-#include <libqhullcpp/QhullError.h>
+//#include <libqhullcpp/RboxPoints.h>
+//#include <libqhullcpp/QhullError.h>
 #include <libqhullcpp/QhullQh.h>
 #include <libqhullcpp/QhullFacetList.h>
-#include <libqhullcpp/QhullLinkedList.h>
+//#include <libqhullcpp/QhullLinkedList.h>
 #include <libqhullcpp/Qhull.h>
 
 #include <cilantro/point_cloud.hpp>
 
-void VtoH(const std::vector<Eigen::Vector3f> &points);
+template <class VectorInT, class FaceVectorOutT>
+std::vector<FaceVectorOutT> VtoH(const std::vector<VectorInT> &points) {
+    if (points.empty()) return std::vector<FaceVectorOutT>(0);
 
-void HtoV(const std::vector<Eigen::Vector4f> &faces, const Eigen::Vector3f &interior_point);
+    size_t dim = points[0].size();
+    Eigen::Matrix<realT, Eigen::Dynamic, Eigen::Dynamic> data(
+            Eigen::Map<Eigen::Matrix<typename VectorInT::Scalar, Eigen::Dynamic, Eigen::Dynamic> >((typename VectorInT::Scalar *)points.data(), dim, points.size()).template cast<realT>()
+    );
+
+    orgQhull::Qhull q("", dim, points.size(), data.data(), "");
+    orgQhull::QhullFacetList facets = q.facetList();
+
+    size_t k = 0;
+    std::vector<FaceVectorOutT> res(facets.size());
+    for (auto fi = facets.begin(); fi != facets.end(); ++fi) {
+        Eigen::Matrix<coordT, Eigen::Dynamic, 1> hp(dim+1);
+        size_t i = 0;
+        for (auto hpi = fi->hyperplane().begin(); hpi != fi->hyperplane().end(); ++hpi) {
+            hp(i++) = *hpi;
+        }
+        hp(dim) = fi->hyperplane().offset();
+        res[k++] = hp.cast<typename FaceVectorOutT::Scalar>();
+    }
+
+    return res;
+}
+
+template <class FaceVectorInT, class VectorInT, class VectorOutT>
+std::vector<VectorOutT> HtoV(const std::vector<FaceVectorInT> &faces, const VectorInT &interior_point) {
+    if (faces.empty()) return std::vector<VectorOutT>(0);
+
+    size_t dim = interior_point.size();
+
+    Eigen::Matrix<realT, Eigen::Dynamic, Eigen::Dynamic> data(
+            Eigen::Map<Eigen::Matrix<typename FaceVectorInT::Scalar, Eigen::Dynamic, Eigen::Dynamic> >((typename FaceVectorInT::Scalar *)faces.data(), dim+1, faces.size()).template cast<realT>()
+    );
+
+    Eigen::Matrix<coordT, Eigen::Dynamic, 1> feasible_point(interior_point.template cast<coordT>());
+    std::vector<coordT> fpv(dim);
+    Eigen::Matrix<coordT, Eigen::Dynamic, 1>::Map(fpv.data(), dim) = feasible_point;
+
+    orgQhull::Qhull q;
+    q.setFeasiblePoint(orgQhull::Coordinates(fpv));
+    q.qh()->HALFspace = True;
+    q.runQhull("", dim+1, faces.size(), data.data(), "");
+    orgQhull::QhullFacetList facets = q.facetList();
+
+    size_t k = 0;
+    std::vector<VectorOutT> res(facets.size());
+    for (auto fi = facets.begin(); fi != facets.end(); ++fi) {
+        Eigen::Matrix<coordT, Eigen::Dynamic, 1> normal(dim);
+        size_t i = 0;
+        for (auto hpi = fi->hyperplane().begin(); hpi != fi->hyperplane().end(); ++hpi) {
+            normal(i++) = *hpi;
+        }
+//        hp(dim) = fi->hyperplane().offset();
+        res[k++] = (-normal/fi->hyperplane().offset() + feasible_point).cast<typename VectorOutT::Scalar>();
+    }
+
+    return res;
+
+//    for (k=qh->hull_dim; k--; )
+//        *(coordp++)= (*(normp++) / - facet->offset) + *(feasiblep++);
+
+}
