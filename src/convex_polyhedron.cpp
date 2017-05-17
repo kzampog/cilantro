f2885908f1dfc5da09db1946e4d601b59b994993
Konstantinos Zampogiannis <kzampog@gmail.com>
2017-05-16 22:08:03 -0400

    Working with Qhull...

--- src/convex_polyhedron.cpp
+++ src/convex_polyhedron.cpp
@@ -9,12 +9,13 @@ void VtoH(const std::vector<Eigen::Vector3f> &points) {
     orgQhull::Qhull q("", dim, points.size(), data.data(), "");
     orgQhull::QhullFacetList facets = q.facetList();
     std::cout << facets;
+
+    std::cout << q.area() << std::endl;
 }
 
 void HtoV(const std::vector<Eigen::Vector4f> &faces) {
     size_t dim = 4;
     Eigen::MatrixXd data = Eigen::Map<Eigen::MatrixXf>((float *)faces.data(), dim, faces.size()).cast<realT>();
-
 
     std::vector<coordT> fp;
     fp.push_back(0.2);
@@ -24,6 +25,10 @@ void HtoV(const std::vector<Eigen::Vector4f> &faces) {
     orgQhull::Qhull q;
     q.setFeasiblePoint(orgQhull::Coordinates(fp));
     q.runQhull("", dim, faces.size(), data.data(), "H");
+    orgQhull::QhullPoints points = q.points();
+    std::cout << points;
     orgQhull::QhullVertexList vertices = q.vertexList();
     std::cout << vertices;
+
+    std::cout << q.area() << std::endl;
 }
