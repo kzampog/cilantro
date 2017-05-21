9a208d33dc814a17d051a8e348a66c54447639b8
Konstantinos Zampogiannis <kzampog@gmail.com>
2017-05-21 13:58:27 -0400

    qhull fixes

--- include/cilantro/convex_hull.hpp
+++ include/cilantro/convex_hull.hpp
@@ -6,16 +6,14 @@
 #include <libqhullcpp/Qhull.h>
 #include <libqhullcpp/QhullVertexSet.h>
 
-//#include <eigen_quadprog/eiquadprog.hpp>
+#include <eigen_quadprog/eiquadprog.hpp>
 
 #include <cilantro/point_cloud.hpp>
 
 template <class PointInDataT, class PointOutT, class HalfspaceOutT>
 void VtoH(PointInDataT * points, size_t dim, size_t num_points, std::vector<PointOutT> &hull_points, std::vector<HalfspaceOutT> &halfspaces, std::vector<std::vector<size_t> > &faces, std::vector<size_t> &hull_point_indices, bool simplicial_faces = true) {
 
-    Eigen::Matrix<realT, Eigen::Dynamic, Eigen::Dynamic> data(
-            Eigen::Map<Eigen::Matrix<PointInDataT, Eigen::Dynamic, Eigen::Dynamic> >((PointInDataT *)points, dim, num_points).template cast<realT>()
-    );
+    Eigen::Matrix<realT, Eigen::Dynamic, Eigen::Dynamic> data(Eigen::Map<Eigen::Matrix<PointInDataT, Eigen::Dynamic, Eigen::Dynamic> >(points, dim, num_points).template cast<realT>());
 
     orgQhull::Qhull qh;
     if (simplicial_faces) qh.qh()->TRIangulate = True;
@@ -82,10 +80,8 @@ void VtoH(const std::vector<PointInT> &points, std::vector<PointOutT> &hull_poin
 }
 
 template <class HalfspaceInDataT, class PointInT, class PointOutT>
-void HtoV(HalfspaceInDataT * halfspaces, size_t dim, size_t num_halfspaces, const PointInT &interior_point, std::vector<PointOutT> &hull_points) {
-    Eigen::Matrix<realT, Eigen::Dynamic, Eigen::Dynamic> data(
-            Eigen::Map<Eigen::Matrix<HalfspaceInDataT, Eigen::Dynamic, Eigen::Dynamic> >((HalfspaceInDataT *)halfspaces, dim+1, num_halfspaces).template cast<realT>()
-    );
+void HtoV(HalfspaceInDataT * halfspaces, size_t dim, size_t num_halfspaces, const PointInT &interior_point, std::vector<PointOutT> &hull_points, realT merge_tol = 0.000001) {
+    Eigen::Matrix<realT, Eigen::Dynamic, Eigen::Dynamic> data(Eigen::Map<Eigen::Matrix<HalfspaceInDataT, Eigen::Dynamic, Eigen::Dynamic> >(halfspaces, dim+1, num_halfspaces).template cast<realT>());
 
     Eigen::Matrix<coordT, Eigen::Dynamic, 1> feasible_point(interior_point.head(dim).template cast<coordT>());
     std::vector<coordT> fpv(dim);
@@ -93,10 +89,12 @@ void HtoV(HalfspaceInDataT * halfspaces, size_t dim, size_t num_halfspaces, cons
 
     orgQhull::Qhull qh;
     qh.qh()->HALFspace = True;
+    qh.qh()->premerge_centrum = merge_tol;
     qh.setFeasiblePoint(orgQhull::Coordinates(fpv));
     qh.runQhull("", dim+1, num_halfspaces, data.data(), "");
     orgQhull::QhullFacetList facets = qh.facetList();
 
+    // Get hull points from dual facets
     size_t k = 0;
     hull_points.resize(facets.size());
     for (auto fi = facets.begin(); fi != facets.end(); ++fi) {
@@ -110,65 +108,43 @@ void HtoV(HalfspaceInDataT * halfspaces, size_t dim, size_t num_halfspaces, cons
 }
 
 template <class HalfspaceInT, class PointInT, class PointOutT>
-void HtoV(const std::vector<HalfspaceInT> &halfspaces, const PointInT &interior_point, std::vector<PointOutT> &hull_points) {
+void HtoV(const std::vector<HalfspaceInT> &halfspaces, const PointInT &interior_point, std::vector<PointOutT> &hull_points, realT merge_tol = 0.000001) {
     if (halfspaces.empty()) return;
-    HtoV<typename HalfspaceInT::Scalar,PointInT,PointOutT>((typename HalfspaceInT::Scalar *)halfspaces.data(), halfspaces[0].size()-1, halfspaces.size(), interior_point, hull_points);
+    HtoV<typename HalfspaceInT::Scalar,PointInT,PointOutT>((typename HalfspaceInT::Scalar *)halfspaces.data(), halfspaces[0].size()-1, halfspaces.size(), interior_point, hull_points, merge_tol);
 }
 
-//template <class HalfspaceInDataT, class PointOutT>
-//void HtoV(HalfspaceInDataT * halfspaces, size_t dim, size_t num_halfspaces, std::vector<PointOutT> &hull_points) {
-//    // Find a feasible point with LP
-//
-////    Eigen::MatrixXd G(Eigen::MatrixXd::Zero(dim+2,dim+2));
-//    Eigen::MatrixXd G(Eigen::MatrixXd::Identity(dim+2,dim+2)/1000000.0);
-//    Eigen::VectorXd g0(Eigen::VectorXd::Zero(dim+2));
-//    g0(dim+1) = -1.0;
-//
-//    Eigen::MatrixXd CE(dim+2,0);
-//    Eigen::VectorXd ce0(0);
-//
-//    Eigen::MatrixXd ineq_data(Eigen::Map<Eigen::Matrix<HalfspaceInDataT,Eigen::Dynamic,Eigen::Dynamic> >((HalfspaceInDataT *)halfspaces, dim+1, num_halfspaces).template cast<double>());
-//    Eigen::MatrixXd CI(dim+2,num_halfspaces);
-//    CI.topRows(dim+1) = ineq_data;
-//    CI.row(dim+1) = Eigen::VectorXd::Ones(num_halfspaces);
-//    Eigen::VectorXd ci0(Eigen::VectorXd::Zero(num_halfspaces));
-//
-//    Eigen::VectorXd x(dim+2);
-//
-//    double opt_val = solve_quadprog(G, g0, CE, ce0, CI, ci0, x);
-//
-//    std::cout << "G" << std::endl;
-//    std::cout << G << std::endl;
-//
-//    std::cout << "g0" << std::endl;
-//    std::cout << g0 << std::endl;
-//
-//    std::cout << "CE" << std::endl;
-//    std::cout << CE << std::endl;
-//
-//    std::cout << "ce0" << std::endl;
-//    std::cout << ce0 << std::endl;
-//
-//    std::cout << "CI" << std::endl;
-//    std::cout << CI << std::endl;
-//
-//    std::cout << "ci0" << std::endl;
-//    std::cout << ci0 << std::endl;
-//
-//
-//    std::cout << "opt f is: " << opt_val << std::endl;
-//    std::cout << "opt x is: " << x.transpose() << std::endl;
-//
-//
-//    Eigen::VectorXd feasible_point(dim);
-//
-//
-//    Eigen::Matrix<HalfspaceInDataT,Eigen::Dynamic,1> interior_point(feasible_point.cast<HalfspaceInDataT>());
-//    HtoV<HalfspaceInDataT,Eigen::Matrix<HalfspaceInDataT,Eigen::Dynamic,1>,PointOutT>(halfspaces, dim, num_halfspaces, interior_point, hull_points);
-//}
-//
-//template <class HalfspaceInT, class PointOutT>
-//void HtoV(const std::vector<HalfspaceInT> &halfspaces, std::vector<PointOutT> &hull_points) {
-//    if (halfspaces.empty()) return;
-//    HtoV<typename HalfspaceInT::Scalar,PointOutT>((typename HalfspaceInT::Scalar *)halfspaces.data(), halfspaces[0].size()-1, halfspaces.size(), hull_points);
-//}
+template <class HalfspaceInDataT, class PointOutT>
+void HtoV(HalfspaceInDataT * halfspaces, size_t dim, size_t num_halfspaces, std::vector<PointOutT> &hull_points, realT merge_tol = 0.000001) {
+    // Find a feasible point
+    // Objective
+    //Eigen::MatrixXd G(Eigen::MatrixXd::Zero(dim+2,dim+2));
+    Eigen::MatrixXd G(Eigen::MatrixXd::Identity(dim+2,dim+2));
+    Eigen::VectorXd g0(Eigen::VectorXd::Zero(dim+2));
+    g0(dim+1) = -1.0;
+
+    // Equality constraints
+    Eigen::MatrixXd CE(dim+2,0);
+    Eigen::VectorXd ce0(0);
+
+    // Inequality constraints
+    Eigen::MatrixXd ineq_data(Eigen::Map<Eigen::Matrix<HalfspaceInDataT,Eigen::Dynamic,Eigen::Dynamic> >((HalfspaceInDataT *)halfspaces, dim+1, num_halfspaces).template cast<double>());
+    Eigen::MatrixXd CI(dim+2,num_halfspaces);
+    CI.topRows(dim+1) = -ineq_data;
+    CI.row(dim+1) = -Eigen::VectorXd::Ones(num_halfspaces);
+    Eigen::VectorXd ci0(Eigen::VectorXd::Zero(num_halfspaces));
+
+    // Optimization
+    Eigen::VectorXd x(dim+2);
+    solve_quadprog(G, g0, CE, ce0, CI, ci0, x);
+
+    Eigen::VectorXd feasible_point(x.head(dim)/x(dim));
+    Eigen::Matrix<HalfspaceInDataT,Eigen::Dynamic,1> interior_point(feasible_point.cast<HalfspaceInDataT>());
+
+    HtoV<HalfspaceInDataT,Eigen::Matrix<HalfspaceInDataT,Eigen::Dynamic,1>,PointOutT>(halfspaces, dim, num_halfspaces, interior_point, hull_points, merge_tol);
+}
+
+template <class HalfspaceInT, class PointOutT>
+void HtoV(const std::vector<HalfspaceInT> &halfspaces, std::vector<PointOutT> &hull_points, realT merge_tol = 0.000001) {
+    if (halfspaces.empty()) return;
+    HtoV<typename HalfspaceInT::Scalar,PointOutT>((typename HalfspaceInT::Scalar *)halfspaces.data(), halfspaces[0].size()-1, halfspaces.size(), hull_points, merge_tol);
+}
