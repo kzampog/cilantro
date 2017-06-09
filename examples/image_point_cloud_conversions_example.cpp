7e641d0b053f13c691d8741e667af83022eb75cd
Konstantinos Zampogiannis <kzampog@gmail.com>
2017-06-09 18:08:44 -0400

    Updated examples

--- examples/image_point_cloud_conversions_example.cpp
+++ examples/image_point_cloud_conversions_example.cpp
@@ -1,4 +1,5 @@
 #include <cilantro/image_point_cloud_conversions.hpp>
+#include <cilantro/visualizer.hpp>
 #include <cilantro/image_viewer.hpp>
 
 #include <ctime>
@@ -10,14 +11,47 @@ int main( int argc, char* argv[] )
 
     std::unique_ptr<pangolin::VideoInterface> dok = pangolin::OpenVideo(uri);
 
-    ImageViewer rgbv("RGB", "disp1");
-    ImageViewer depthv("DEPTH", "disp2");
+    Visualizer pcdv("CLOUD", "disp");
+    ImageViewer rgbv("RGB", "disp");
+    ImageViewer depthv("DEPTH", "disp");
+
+    Eigen::Matrix3f K;
+    K << 528, 0, 320, 0, 528, 240, 0, 0, 1;
 
     size_t w = 640, h = 480;
+    PointCloud cloud;
     unsigned char* img = new unsigned char[dok->SizeBytes()];
-    while (dok->GrabNext(img, true) && !rgbv.wasStopped() && !depthv.wasStopped()) {
-        rgbv.setImage(img, w, h, "RGB24").spinOnce();
-        depthv.setImage(img + 3*w*h, w, h, "GRAY16LE").spinOnce();
+    while (dok->GrabNext(img, true)) {
+        pangolin::Image<Eigen::Matrix<unsigned char,3,1> > rgb_img((Eigen::Matrix<unsigned char,3,1> *)img, w, h, w*sizeof(Eigen::Matrix<unsigned char,3,1>));
+        pangolin::Image<unsigned short> depth_img((unsigned short *)(img+3*w*h), w, h, w*sizeof(unsigned short));
+
+        pangolin::ManagedImage<Eigen::Matrix<unsigned char,3,1> > rgb_img2(w,h);
+        pangolin::ManagedImage<unsigned short> depth_img2(w,h);
+
+        clock_t begin, end;
+        double build_time;
+        begin = clock();
+
+        RGBDImagesToPointCloud(rgb_img, depth_img, K, cloud, false);
+//        depthImageToPointCloud(depth_img, K, cloud, false);
+
+//        Eigen::Matrix3f R;
+//        R.setIdentity();
+//        R(0,0) = -1.0f;
+//        R(1,1) = -1.0f;
+//        Eigen::Vector3f t(0,0,0);
+
+//        pointCloudToRGBDImages(cloud, K, R, t, rgb_img2, depth_img2);
+        pointCloudToRGBDImages(cloud, K, rgb_img2, depth_img2);
+//        pointCloudToDepthImage(cloud, K, depth_img2);
+
+        end = clock();
+        build_time = 1000.0*double(end - begin) / CLOCKS_PER_SEC;
+        std::cout << "Images to cloud and back: " << build_time << std::endl;
+
+        pcdv.addPointCloud("cloud", cloud).spinOnce();
+        rgbv.setImage(rgb_img2.ptr, w, h, "RGB24").spinOnce();
+        depthv.setImage(depth_img2.ptr, w, h, "GRAY16LE").spinOnce();
     }
 
     delete[] img;
