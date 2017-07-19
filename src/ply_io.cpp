8617a38a947699c5378ac06227b188cfe885b6e4
Kanishka Ganguly <kganguly@umd.edu>
2017-07-19 16:40:59 -0400

    Fixed PLY writing

--- src/ply_io.cpp
+++ src/ply_io.cpp
@@ -22,14 +22,14 @@ void readPointCloudFromPLYFile(const std::string &filename, PointCloud &cloud) {
 
         // Populate cloud
         cloud.points.resize(vertex_count);
-        std::memcpy(cloud.points.data(), vertex_data.data(), 3*vertex_count*sizeof(float));
+        std::memcpy(cloud.points.data(), vertex_data.data(), 3 * vertex_count * sizeof(float));
 
         cloud.normals.resize(normal_count);
-        std::memcpy(cloud.normals.data(), normal_data.data(), 3*normal_count*sizeof(float));
+        std::memcpy(cloud.normals.data(), normal_data.data(), 3 * normal_count * sizeof(float));
 
         cloud.colors.resize(color_count);
         for (int i = 0; i < color_count; i++) {
-            cloud.colors[i] = Eigen::Vector3f(color_data[3*i], color_data[3*i+1], color_data[3*i+2])/255.0f;
+            cloud.colors[i] = Eigen::Vector3f(color_data[3 * i], color_data[3 * i + 1], color_data[3 * i + 2]) / 255.0f;
         }
 
     } catch (const std::exception &e) {
@@ -38,19 +38,10 @@ void readPointCloudFromPLYFile(const std::string &filename, PointCloud &cloud) {
 }
 
 void writePointCloudToPLYFile(const std::string &filename, const PointCloud &cloud) {
-    // Populate data holders
-    std::vector<float> vertex_data(3*cloud.size());
-    std::memcpy(vertex_data.data(), cloud.points.data(), 3*cloud.size()*sizeof(float));
+    if (cloud.empty()) return;
 
-    std::vector<float> normal_data(3*cloud.size());
-    std::memcpy(normal_data.data(), cloud.normals.data(), 3*cloud.size()*sizeof(float));
-
-    std::vector<uint8_t> color_data(3*cloud.size());
-    for (int i = 0; i < cloud.size(); i++) {
-        color_data[3*i + 0] = (uint8_t)(cloud.colors[i](0)*255.0f);
-        color_data[3*i + 1] = (uint8_t)(cloud.colors[i](1)*255.0f);
-        color_data[3*i + 2] = (uint8_t)(cloud.colors[i](2)*255.0f);
-    }
+    std::vector<float> vertex_data(3 * cloud.size());
+    std::memcpy(vertex_data.data(), cloud.points.data(), 3 * cloud.size() * sizeof(float));
 
     // Write to file
     std::filebuf fb;
@@ -58,12 +49,24 @@ void writePointCloudToPLYFile(const std::string &filename, const PointCloud &clo
     std::ostream outputStream(&fb);
 
     tinyply::PlyFile file_to_write;
-
     file_to_write.add_properties_to_element("vertex", {"x", "y", "z"}, vertex_data);
-    file_to_write.add_properties_to_element("vertex", {"nx", "ny", "nz"}, normal_data);
-    file_to_write.add_properties_to_element("vertex", {"red", "green", "blue"}, color_data);
+
+    if (cloud.hasNormals()) {
+        std::vector<float> normal_data(3 * cloud.normals.size());
+        std::memcpy(normal_data.data(), cloud.normals.data(), 3 * cloud.normals.size() * sizeof(float));
+        file_to_write.add_properties_to_element("vertex", {"nx", "ny", "nz"}, normal_data);
+    }
+
+    if (cloud.hasColors()) {
+        std::vector<uint8_t> color_data(3 * cloud.colors.size());
+        for (int i = 0; i < cloud.colors.size(); i++) {
+            color_data[3 * i + 0] = (uint8_t) (cloud.colors[i](0) * 255.0f);
+            color_data[3 * i + 1] = (uint8_t) (cloud.colors[i](1) * 255.0f);
+            color_data[3 * i + 2] = (uint8_t) (cloud.colors[i](2) * 255.0f);
+        }
+        file_to_write.add_properties_to_element("vertex", {"red", "green", "blue"}, color_data);
+    }
 
     file_to_write.write(outputStream, true);
-
     fb.close();
 }
