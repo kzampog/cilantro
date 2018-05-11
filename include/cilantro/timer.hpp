5920d099bbc0acaff78bc04a8fa20e3449637485
Konstantinos Zampogiannis <kzampog@gmail.com>
2018-05-11 18:44:03 -0400

    Minor additions

--- include/cilantro/timer.hpp
+++ include/cilantro/timer.hpp
@@ -7,15 +7,25 @@ namespace cilantro {
     public:
         typedef decltype(std::chrono::high_resolution_clock::now()) TimePoint;
 
-        inline Timer() {}
+        inline Timer(bool start = false) { if (start) start_time_ = std::chrono::high_resolution_clock::now(); }
 
         inline Timer& start() { start_time_ = std::chrono::high_resolution_clock::now(); return *this; }
 
         inline Timer& stop() { stop_time_ = std::chrono::high_resolution_clock::now(); return *this; }
 
         template <class Period = std::milli>
+        inline double getElapsedTimeSinceStart() const {
+            return std::chrono::duration<double,Period>(std::chrono::high_resolution_clock::now() - start_time_).count();
+        }
+
+        template <class Period = std::milli>
         inline double getElapsedTime() const {
             return std::chrono::duration<double,Period>(stop_time_ - start_time_).count();
+        }
+
+        template <class Period = std::milli>
+        inline double stopAndGetElapsedTime() {
+            return stop().getElapsedTime();
         }
 
     private:
