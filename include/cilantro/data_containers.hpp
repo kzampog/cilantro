c14c2c3fd88eea0d396e835c61030663adc5b863
Konstantinos Zampogiannis <kzampog@gmail.com>
2018-05-15 19:46:07 -0400

    Added convenience typedefs

--- include/cilantro/data_containers.hpp
+++ include/cilantro/data_containers.hpp
@@ -47,8 +47,17 @@ namespace cilantro {
                 : Eigen::Map<Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>>(data, dim, num_points)
         {}
 
-        inline Eigen::Map<Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>>& eigenMap() { return (*(Eigen::Map<Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>> *)this); }
+        inline Eigen::Map<Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>>& eigenMap() {
+            return (*(Eigen::Map<Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>> *)this);
+        }
     };
+
+    typedef DataMatrixMap<float,2> DataMatrixMap2f;
+    typedef DataMatrixMap<double,2> DataMatrixMap2d;
+    typedef DataMatrixMap<float,3> DataMatrixMap3f;
+    typedef DataMatrixMap<double,3> DataMatrixMap3d;
+    typedef DataMatrixMap<float,Eigen::Dynamic> DataMatrixMapXf;
+    typedef DataMatrixMap<double,Eigen::Dynamic> DataMatrixMapXd;
 
     // Read-only Eigen Map (for inputs)
     template <typename ScalarT, ptrdiff_t EigenDim>
@@ -98,24 +107,75 @@ namespace cilantro {
                 : Eigen::Map<const Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>>(data, dim, num_points)
         {}
 
-        inline Eigen::Map<const Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>>& eigenMap() { return (*(Eigen::Map<const Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>> *)this); }
+        inline Eigen::Map<const Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>>& eigenMap() {
+            return (*(Eigen::Map<const Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>> *)this);
+        }
     };
+
+    typedef ConstDataMatrixMap<float,2> ConstDataMatrixMap2f;
+    typedef ConstDataMatrixMap<double,2> ConstDataMatrixMap2d;
+    typedef ConstDataMatrixMap<float,3> ConstDataMatrixMap3f;
+    typedef ConstDataMatrixMap<double,3> ConstDataMatrixMap3d;
+    typedef ConstDataMatrixMap<float,Eigen::Dynamic> ConstDataMatrixMapXf;
+    typedef ConstDataMatrixMap<double,Eigen::Dynamic> ConstDataMatrixMapXd;
 
     template <typename ScalarT, ptrdiff_t EigenDim>
     using ConstVectorSetMatrixMap = ConstDataMatrixMap<ScalarT,EigenDim>;
 
+    typedef ConstVectorSetMatrixMap<float,2> ConstVectorSetMatrixMap2f;
+    typedef ConstVectorSetMatrixMap<double,2> ConstVectorSetMatrixMap2d;
+    typedef ConstVectorSetMatrixMap<float,3> ConstVectorSetMatrixMap3f;
+    typedef ConstVectorSetMatrixMap<double,3> ConstVectorSetMatrixMap3d;
+    typedef ConstVectorSetMatrixMap<float,Eigen::Dynamic> ConstVectorSetMatrixMapXf;
+    typedef ConstVectorSetMatrixMap<double,Eigen::Dynamic> ConstVectorSetMatrixMapXd;
+
     template <typename ScalarT, ptrdiff_t EigenDim>
     using ConstHomogeneousVectorSetMatrixMap = typename std::conditional<EigenDim == Eigen::Dynamic, ConstDataMatrixMap<ScalarT,EigenDim>, ConstDataMatrixMap<ScalarT,EigenDim+1>>::type;
+
+    typedef ConstHomogeneousVectorSetMatrixMap<float,2> ConstHomogeneousVectorSetMatrixMap2f;
+    typedef ConstHomogeneousVectorSetMatrixMap<double,2> ConstHomogeneousVectorSetMatrixMap2d;
+    typedef ConstHomogeneousVectorSetMatrixMap<float,3> ConstHomogeneousVectorSetMatrixMap3f;
+    typedef ConstHomogeneousVectorSetMatrixMap<double,3> ConstHomogeneousVectorSetMatrixMap3d;
+    typedef ConstHomogeneousVectorSetMatrixMap<float,Eigen::Dynamic> ConstHomogeneousVectorSetMatrixMapXf;
+    typedef ConstHomogeneousVectorSetMatrixMap<double,Eigen::Dynamic> ConstHomogeneousVectorSetMatrixMapXd;
 
     template <typename ScalarT, ptrdiff_t EigenDim>
     using Vector = Eigen::Matrix<ScalarT,EigenDim,1>;
 
+    typedef Vector<float,2> Vector2f;
+    typedef Vector<double,2> Vector2d;
+    typedef Vector<float,3> Vector3f;
+    typedef Vector<double,3> Vector3d;
+    typedef Vector<float,Eigen::Dynamic> VectorXf;
+    typedef Vector<double,Eigen::Dynamic> VectorXd;
+
     template <typename ScalarT, ptrdiff_t EigenDim>
     using VectorSet = Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>;
+
+    typedef VectorSet<float,2> VectorSet2f;
+    typedef VectorSet<double,2> VectorSet2d;
+    typedef VectorSet<float,3> VectorSet3f;
+    typedef VectorSet<double,3> VectorSet3d;
+    typedef VectorSet<float,Eigen::Dynamic> VectorSetXf;
+    typedef VectorSet<double,Eigen::Dynamic> VectorSetXd;
 
     template <typename ScalarT, ptrdiff_t EigenDim>
     using HomogeneousVector = typename std::conditional<EigenDim == Eigen::Dynamic, Eigen::Matrix<ScalarT,EigenDim,1>, Eigen::Matrix<ScalarT,EigenDim+1,1>>::type;
 
+    typedef HomogeneousVector<float,2> HomogeneousVector2f;
+    typedef HomogeneousVector<double,2> HomogeneousVector2d;
+    typedef HomogeneousVector<float,3> HomogeneousVector3f;
+    typedef HomogeneousVector<double,3> HomogeneousVector3d;
+    typedef HomogeneousVector<float,Eigen::Dynamic> HomogeneousVectorXf;
+    typedef HomogeneousVector<double,Eigen::Dynamic> HomogeneousVectorXd;
+
     template <typename ScalarT, ptrdiff_t EigenDim>
     using HomogeneousVectorSet = typename std::conditional<EigenDim == Eigen::Dynamic, Eigen::Matrix<ScalarT,EigenDim,Eigen::Dynamic>, Eigen::Matrix<ScalarT,EigenDim+1,Eigen::Dynamic>>::type;
+
+    typedef HomogeneousVectorSet<float,2> HomogeneousVectorSet2f;
+    typedef HomogeneousVectorSet<double,2> HomogeneousVectorSet2d;
+    typedef HomogeneousVectorSet<float,3> HomogeneousVectorSet3f;
+    typedef HomogeneousVectorSet<double,3> HomogeneousVectorSet3d;
+    typedef HomogeneousVectorSet<float,Eigen::Dynamic> HomogeneousVectorSetXf;
+    typedef HomogeneousVectorSet<double,Eigen::Dynamic> HomogeneousVectorSetXd;
 }
