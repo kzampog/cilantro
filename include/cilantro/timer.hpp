4cbab7fb18853cdec7b903a9d05a0732538f51b9
Konstantinos Zampogiannis <kzampog@gmail.com>
2018-05-21 18:52:42 -0400

    Updates

--- include/cilantro/timer.hpp
+++ include/cilantro/timer.hpp
@@ -5,31 +5,29 @@
 namespace cilantro {
     class Timer {
     public:
-        typedef decltype(std::chrono::high_resolution_clock::now()) TimePoint;
-
         inline Timer(bool start = false) { if (start) start_time_ = std::chrono::high_resolution_clock::now(); }
 
         inline Timer& start() { start_time_ = std::chrono::high_resolution_clock::now(); return *this; }
 
         inline Timer& stop() { stop_time_ = std::chrono::high_resolution_clock::now(); return *this; }
 
-        template <class Period = std::milli>
-        inline double getElapsedTimeSinceStart() const {
-            return std::chrono::duration<double,Period>(std::chrono::high_resolution_clock::now() - start_time_).count();
+        template <class DurationT = std::chrono::duration<double,std::milli>>
+        inline typename DurationT::rep getElapsedTimeSinceStart() const {
+            return std::chrono::duration_cast<DurationT>(std::chrono::high_resolution_clock::now() - start_time_).count();
         }
 
-        template <class Period = std::milli>
-        inline double getElapsedTime() const {
-            return std::chrono::duration<double,Period>(stop_time_ - start_time_).count();
+        template <class DurationT = std::chrono::duration<double,std::milli>>
+        inline typename DurationT::rep getElapsedTime() const {
+            return std::chrono::duration_cast<DurationT>(stop_time_ - start_time_).count();
         }
 
-        template <class Period = std::milli>
-        inline double stopAndGetElapsedTime() {
-            return stop().getElapsedTime();
+        template <class DurationT = std::chrono::duration<double,std::milli>>
+        inline typename DurationT::rep stopAndGetElapsedTime() {
+            return stop().template getElapsedTime<DurationT>();
         }
 
     private:
-        TimePoint start_time_;
-        TimePoint stop_time_;
+        std::chrono::time_point<std::chrono::high_resolution_clock> start_time_;
+        std::chrono::time_point<std::chrono::high_resolution_clock> stop_time_;
     };
 }
