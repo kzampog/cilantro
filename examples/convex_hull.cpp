5bd7d7e38755eea3ee065b484a3ac49d8f075cb3
Konstantinos Zampogiannis <kzampog@gmail.com>
2018-05-27 20:49:50 -0400

    Examples improvements

--- examples/convex_hull.cpp
+++ examples/convex_hull.cpp
@@ -1,114 +1,95 @@
-#include <cilantro/flat_convex_hull_3d.hpp>
+#include <cilantro/convex_polytope.hpp>
 #include <cilantro/point_cloud.hpp>
 #include <cilantro/visualizer.hpp>
 #include <cilantro/common_renderables.hpp>
 
+void run_demo() {
+    std::vector<Eigen::Vector3f> points;
+    points.emplace_back(0,0,0);
+    points.emplace_back(1,0,0);
+    points.emplace_back(0,1,0);
+    points.emplace_back(0,0,1);
+    points.emplace_back(0,1,1);
+    points.emplace_back(1,0,1);
+    points.emplace_back(1,1,0);
+    points.emplace_back(1,1,1);
+    points.emplace_back(0.5,0.5,0.5);
+
+    cilantro::ConvexHull3f ch(points, true);
+
+    std::cout << "Vertices:" << std::endl;
+    for (size_t i = 0; i < ch.getVertices().cols(); i++) {
+        std::cout << ch.getVertices().col(i).transpose() << std::endl;
+    }
+
+    std::cout << "Faces:" << std::endl;
+    for (size_t i = 0; i < ch.getFacetVertexIndices().size(); i++) {
+        for (size_t j = 0; j < ch.getFacetVertexIndices()[i].size(); j++) {
+            std::cout << ch.getFacetVertexIndices()[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Vertex neighbor faces:" << std::endl;
+    for (size_t i = 0; i < ch.getVertexNeighborFacets().size(); i++) {
+        for (size_t j = 0; j < ch.getVertexNeighborFacets()[i].size(); j++) {
+            std::cout << ch.getVertexNeighborFacets()[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Face neighbor faces:" << std::endl;
+    for (size_t i = 0; i < ch.getFacetNeighborFacets().size(); i++) {
+        for (size_t j = 0; j < ch.getFacetNeighborFacets()[i].size(); j++) {
+            std::cout << ch.getFacetNeighborFacets()[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, char ** argv) {
-//    std::vector<Eigen::Vector3f> points;
-//    points.push_back(Eigen::Vector3f(0,0,0));
-//    points.push_back(Eigen::Vector3f(1,0,0));
-//    points.push_back(Eigen::Vector3f(0,1,0));
-//    points.push_back(Eigen::Vector3f(0,0,1));
-//    points.push_back(Eigen::Vector3f(0,1,1));
-//    points.push_back(Eigen::Vector3f(1,0,1));
-//    points.push_back(Eigen::Vector3f(1,1,0));
-//    points.push_back(Eigen::Vector3f(1,1,1));
-//    points.push_back(Eigen::Vector3f(0.5,0.5,0.5));
-//
-//    ConvexHull3D ch(points);
-//
-//    std::cout << "Vertices:" << std::endl;
-//    for (size_t i = 0; i < ch.getVertices().size(); i++) {
-//        std::cout << ch.getVertices()[i].transpose() << std::endl;
-//    }
-//
-//    std::cout << "Faces:" << std::endl;
-//    for (size_t i = 0; i < ch.getFacetVertexIndices().size(); i++) {
-//        for (size_t j = 0; j < ch.getFacetVertexIndices()[i].size(); j++) {
-//            std::cout << ch.getFacetVertexIndices()[i][j] << " ";
-//        }
-//        std::cout << std::endl;
-//    }
-//
-//    std::cout << "Vertex neighbor faces:" << std::endl;
-//    for (size_t i = 0; i < ch.getVertexNeighborFacets().size(); i++) {
-//        for (size_t j = 0; j < ch.getVertexNeighborFacets()[i].size(); j++) {
-//            std::cout << ch.getVertexNeighborFacets()[i][j] << " ";
-//        }
-//        std::cout << std::endl;
-//    }
-//
-//    std::cout << "Face neighbor faces:" << std::endl;
-//    for (size_t i = 0; i < ch.getFacetNeighborFacets().size(); i++) {
-//        for (size_t j = 0; j < ch.getFacetNeighborFacets()[i].size(); j++) {
-//            std::cout << ch.getFacetNeighborFacets()[i][j] << " ";
-//        }
-//        std::cout << std::endl;
-//    }
     if (argc < 2) {
-        std::cout << "Please provide path to PLY file" << std::endl;
+        std::cout << "No input PLY file path provided, running simple demo..." << std::endl;
+        run_demo();
         return 0;
     }
 
     cilantro::PointCloud3f cloud(argv[1]);
 
     cilantro::ConvexHull3f ch(cloud.points, true, true);
-    cilantro::PointCloud3f hc(cloud, ch.getVertexPointIndices());
+    cilantro::PointCloud3f hull_cloud(cloud, ch.getVertexPointIndices());
 
-    std::vector<Eigen::Vector3f> v_colors(ch.getVertices().cols());
-    std::vector<float> v_vals(ch.getVertices().cols());
+    cilantro::VectorSet3f vertex_colors(3, ch.getVertices().cols());
+    std::vector<float> vertex_values(ch.getVertices().cols());
     for (size_t i = 0; i < ch.getVertices().cols(); i++) {
-        if (i%3 == 0) v_colors[i] = Eigen::Vector3f(1,0,0);
-        if (i%3 == 1) v_colors[i] = Eigen::Vector3f(0,1,0);
-        if (i%3 == 2) v_colors[i] = Eigen::Vector3f(0,0,1);
-        v_vals[i] = ch.getVertices().col(i).norm();
+        if (i%3 == 0) vertex_colors.col(i) = Eigen::Vector3f(1,0,0);
+        if (i%3 == 1) vertex_colors.col(i) = Eigen::Vector3f(0,1,0);
+        if (i%3 == 2) vertex_colors.col(i) = Eigen::Vector3f(0,0,1);
+        vertex_values[i] = ch.getVertices().col(i).norm();
     }
 
-    std::vector<Eigen::Vector3f> f_colors(ch.getFacetVertexIndices().size());
-    std::vector<float> f_vals(ch.getFacetVertexIndices().size());
+    cilantro::VectorSet3f face_colors(3, ch.getFacetVertexIndices().size());
+    std::vector<float> face_values(ch.getFacetVertexIndices().size());
     for (size_t i = 0; i < ch.getFacetVertexIndices().size(); i++) {
-        if (i%3 == 0) f_colors[i] = Eigen::Vector3f(1,0,0);
-        if (i%3 == 1) f_colors[i] = Eigen::Vector3f(0,1,0);
-        if (i%3 == 2) f_colors[i] = Eigen::Vector3f(0,0,1);
-        f_vals[i] = (ch.getVertices().col(ch.getFacetVertexIndices()[i][0]) +
+        if (i%3 == 0) face_colors.col(i) = Eigen::Vector3f(1,0,0);
+        if (i%3 == 1) face_colors.col(i) = Eigen::Vector3f(0,1,0);
+        if (i%3 == 2) face_colors.col(i) = Eigen::Vector3f(0,0,1);
+        face_values[i] = (ch.getVertices().col(ch.getFacetVertexIndices()[i][0]) +
                      ch.getVertices().col(ch.getFacetVertexIndices()[i][1]) +
                      ch.getVertices().col(ch.getFacetVertexIndices()[i][2])).rowwise().mean().norm();
     }
 
-    cilantro::Visualizer viz1("3D convex hull", "disp");
+    cilantro::Visualizer viz("3D convex hull", "disp");
+    viz.addObject<cilantro::PointCloudRenderable>("cloud", cloud, cilantro::RenderingProperties().setOpacity(1.0));
+    viz.addObject<cilantro::TriangleMeshRenderable>("mesh", ch.getVertices(), ch.getFacetVertexIndices())
+            ->setVertexNormals(hull_cloud.normals).setVertexColors(vertex_colors).setVertexValues(vertex_values)
+             .setFaceColors(face_colors).setFaceValues(face_values);
 
-    viz1.addObject<cilantro::PointCloudRenderable>("cloud", cloud, cilantro::RenderingProperties().setOpacity(1.0));
-
-    viz1.addObject<cilantro::TriangleMeshRenderable>("mesh", ch.getVertices(), ch.getFacetVertexIndices())
-            ->setVertexNormals(hc.normals).setVertexColors(v_colors).setFaceColors(f_colors).setVertexValues(v_vals).setFaceValues(f_vals);
-
-    cilantro::RenderingProperties rp = viz1.getRenderingProperties("mesh");
+    cilantro::RenderingProperties rp = viz.getRenderingProperties("mesh");
     rp.setUseFaceNormals(true).setUseFaceColors(false).setOpacity(0.8).setColormapType(cilantro::ColormapType::BLUE2RED);
-    viz1.setRenderingProperties("mesh", rp);
+    viz.setRenderingProperties("mesh", rp);
 
-    if (cloud.points.size() < 3) return 0;
-
-    // PointCloudHullFlat also inherits from PrincipalComponentAnalysis
-    cilantro::FlatConvexHull3f ch2d(cloud.points, true, true);
-    cloud.points = ch2d.reconstruct<2>(ch2d.project<2>(cloud.points));
-    cloud.normals.resize(Eigen::NoChange, 0);
-
-    std::vector<std::vector<size_t> > face_v_ind = ch2d.getFacetVertexIndices();
-    std::vector<Eigen::Vector3f> p_src(face_v_ind.size()), p_dst(face_v_ind.size());
-    for (size_t i = 0; i < face_v_ind.size(); i++) {
-        p_src[i] = ch2d.getVertices3D().col(face_v_ind[i][0]);
-        p_dst[i] = ch2d.getVertices3D().col(face_v_ind[i][1]);
-    }
-
-    cilantro::Visualizer viz2("2D convex hull in 3D space", "disp");
-    viz2.addObject<cilantro::PointCloudRenderable>("cloud", cloud, cilantro::RenderingProperties().setOpacity(0.5));
-    viz2.addObject<cilantro::PointCloudRenderable>("hull_cloud", ch2d.getVertices3D(), cilantro::RenderingProperties().setPointColor(1,0,0).setPointSize(10.0));
-    viz2.addObject<cilantro::PointCorrespondencesRenderable>("hull_lines", p_src, p_dst, cilantro::RenderingProperties().setLineColor(0,0,1).setLineWidth(5.0).setLineDensityFraction(1.0));
-
-    while (!viz1.wasStopped() && !viz2.wasStopped()) {
-        viz1.spinOnce();
-        viz2.spinOnce();
-    }
+    viz.spin();
 
     return 0;
 }
---
+++ examples/flat_convex_hull.cpp
@@ -0,0 +1,39 @@
+#include <cilantro/flat_convex_hull_3d.hpp>
+#include <cilantro/point_cloud.hpp>
+#include <cilantro/visualizer.hpp>
+#include <cilantro/common_renderables.hpp>
+
+int main(int argc, char ** argv) {
+    if (argc < 2) {
+        std::cout << "No input PLY file path provided, running simple demo..." << std::endl;
+        return 0;
+    }
+
+    cilantro::PointCloud3f cloud(argv[1]);
+    cloud.normals.resize(3, 0);
+
+    if (cloud.size() < 3) {
+        std::cout << "Input cloud too small" << std::endl;
+        return 0;
+    }
+
+    // PointCloudHullFlat also inherits from PrincipalComponentAnalysis
+    cilantro::FlatConvexHull3f flat_hull(cloud.points, true, true);
+    cloud.points = flat_hull.reconstruct<2>(flat_hull.project<2>(cloud.points));
+
+    const auto& face_v_ind = flat_hull.getFacetVertexIndices();
+    cilantro::VectorSet3f src_points(3, face_v_ind.size());
+    cilantro::VectorSet3f dst_points(3, face_v_ind.size());
+    for (size_t i = 0; i < face_v_ind.size(); i++) {
+        src_points.col(i) = flat_hull.getVertices3D().col(face_v_ind[i][0]);
+        dst_points.col(i) = flat_hull.getVertices3D().col(face_v_ind[i][1]);
+    }
+
+    cilantro::Visualizer viz("2D convex hull in 3D space", "disp");
+    viz.addObject<cilantro::PointCloudRenderable>("cloud", cloud, cilantro::RenderingProperties().setOpacity(0.5));
+    viz.addObject<cilantro::PointCloudRenderable>("hull_cloud", flat_hull.getVertices3D(), cilantro::RenderingProperties().setPointColor(1,0,0).setPointSize(10.0));
+    viz.addObject<cilantro::PointCorrespondencesRenderable>("hull_lines", src_points, dst_points, cilantro::RenderingProperties().setLineColor(0,0,1).setLineWidth(5.0).setLineDensityFraction(1.0));
+    viz.spin();
+
+    return 0;
+}
