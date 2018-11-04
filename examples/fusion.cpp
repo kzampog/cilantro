ced6281b66cbd30478abc901d5d036f9a97d7d2b
Konstantinos Zampogiannis <kzampog@gmail.com>
2018-11-04 00:53:41 -0400

    Example improvements

--- examples/fusion.cpp
+++ examples/fusion.cpp
@@ -150,10 +150,9 @@ int main(int argc, char ** argv) {
                         }
 
                         if (model_pt_ind != empty && std::abs(model_depth - frame_depth) < fusion_dist_thresh) {
+                            // Fuse
                             const float weight = fusion_weight*std::exp(factor*((x - K(0,2))*(x - K(0,2)) + (y - K(1,2))*(y - K(1,2))));
                             const float weight_compl = 1.0f - weight;
-
-                            // Fuse
 #pragma omp critical
                             {
                                 model.points.col(model_pt_ind) = weight_compl*model.points.col(model_pt_ind) + weight*frame_t.points.col(frame_pt_ind);
@@ -174,8 +173,8 @@ int main(int argc, char ** argv) {
         // Visualization
         rgbv.setImage(rgb_img.ptr, w, h, "RGB24");
         pcdv.addObject<cilantro::PointCloudRenderable>("model", model, rp);
-        pcdv.addObject<cilantro::PointCloudRenderable>("frame", frame.transformed(cam_pose), cilantro::RenderingProperties().setOpacity(0.1f).setPointColor(0.8f, 0.8f, 0.8f).setUseLighting(false));
-        pcdv.addObject<cilantro::CameraFrustumRenderable>("cam", w, h, K, cam_pose.matrix(), 0.2f, cilantro::RenderingProperties().setLineWidth(2.0f).setLineColor(1.0f,1.0f,0.0f));
+        pcdv.addObject<cilantro::CameraFrustumRenderable>("cam", w, h, K, cam_pose.matrix(), 0.1f, cilantro::RenderingProperties().setLineWidth(2.0f).setLineColor(1.0f,1.0f,0.0f));
+//        pcdv.addObject<cilantro::PointCloudRenderable>("frame", frame.transformed(cam_pose), cilantro::RenderingProperties().setOpacity(0.2f).setPointColor(0.8f, 0.8f, 0.8f).setUseLighting(false));
 //        pcdv.setCameraPose(cam_pose);
 
         pcdv.clearRenderArea();
