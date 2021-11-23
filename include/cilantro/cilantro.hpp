ef24b3534b4cdaf0bb3a11ffb629da6fda463773
Konstantinos Zampogiannis <kzampog@gmail.com>
2021-11-23 15:30:22 +0200

    Include order fix (qhull issue)

--- include/cilantro/cilantro.hpp
+++ include/cilantro/cilantro.hpp
@@ -5,6 +5,6 @@
 #include <cilantro/correspondence_search.hpp>
 #include <cilantro/model_estimation.hpp>
 #include <cilantro/registration.hpp>
-#include <cilantro/spatial.hpp>
 #include <cilantro/utilities.hpp>
 #include <cilantro/visualization.hpp>
+#include <cilantro/spatial.hpp>
